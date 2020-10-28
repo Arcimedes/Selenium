29a1c48048f37ee64ae9bf193c1d52549e4cc2c8
Artur Dobrzycki <artur.dobrzycki2@gmail.com>
2020-10-28 22:14:41 +0100

    added 2 classes

--- src/test/java/Feature/Login.feature
+++
@@ -1,9 +0,0 @@
-Feature: Login to page
-
-Scenario: Login to page
-  Given I navigate to the login page
-  And I enter to username as admin and password as admin
-  And I click login button
-  Then I should see the userform page
-
-
---
+++ src/test/java/Steps/LogoSteps.java
@@ -0,0 +1,34 @@
+package Steps;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+
+
+public class LogoSteps {
+
+
+    WebDriver driver;
+
+    @Given("^I launch chrome browser$")
+    public void iLaunchChromeBrowser() {
+        System.setProperty("webdriver.chrome.driver", "C://Users//artur//Desktop//Arci//src//test//java");
+
+    }
+
+    @When("^I open orange hrm homepage$")
+    public void iOpenOrangeHrmHomepage() {
+
+    }
+
+    @Then("^I verify that the logo present on page$")
+    public void iVerifyThatTheLogoPresentOnPage() {
+    }
+
+    @And("^close browser$")
+    public void closeBrowser() {
+    }
+}
