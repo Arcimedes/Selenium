74f98e113ba4173c0640c9c1bb09a02e066bcd4b
Artur Dobrzycki <artur.dobrzycki2@gmail.com>
2020-11-12 15:24:07 +0100

    Poprawna konfiguracja

--- src/test/java/projekt/Testrunner2.java
+++ src/test/java/projekt/Testrunner2.java
@@ -3,6 +3,6 @@ package projekt;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features = {"src/test/resources/Feature"}, glue = "Steps")
+@CucumberOptions(features = {"src/test/resources/Feature"}, glue = "projekt/Steps")
 public class Testrunner2 extends AbstractTestNGCucumberTests {
 }
